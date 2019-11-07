21ada36e6bb525d694f9b2f06a83e986f8d42928
bobbida <57205615+bobbida@users.noreply.github.com>
2019-11-08 01:47:18 +0800

    Add files via upload

--- src/tw/yz/domain/Administrator.java
+++ src/tw/yz/domain/Administrator.java
@@ -1,5 +1,24 @@
 package tw.yz.domain;
 
 public class Administrator {
+	private String a_no;
+	private String a_name;
+	
+	public Administrator() {}
+	
+	public String getA_no() {
+		return a_no;
+	}
 
+	public void setA_no(String a_no) {
+		this.a_no = a_no;
+	}
+
+	public String getA_name() {
+		return a_name;
+	}
+
+	public void setA_name(String a_name) {
+		this.a_name = a_name;
+	}
 }
--- src/tw/yz/domain/ExtraName.java
+++ src/tw/yz/domain/ExtraName.java
@@ -1,5 +1,60 @@
 package tw.yz.domain;
 
 public class ExtraName {
+	private int no;
+	private String extra;
+	private String detail;
+	private String name;
+	private String max;
+	private String min;
+	
+	public ExtraName() {}
+	
+	public int getNo() {
+		return no;
+	}
 
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getExtra() {
+		return extra;
+	}
+
+	public void setExtra(String extra) {
+		this.extra = extra;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMax() {
+		return max;
+	}
+
+	public void setMax(String max) {
+		this.max = max;
+	}
+
+	public String getMin() {
+		return min;
+	}
+
+	public void setMin(String min) {
+		this.min = min;
+	}
 }
